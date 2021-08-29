7d7d05ce59426c6bada624b27a6ac5fd2d01fad4
Jan Tuttas <jan.tuttas@msg-david.de>
2021-08-29 13:32:44 +0200

    Step 13: Clear tasks

--- src/components/TaskListButtonBar.vue
+++ src/components/TaskListButtonBar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="buttonBar">
-        <button data-testid="filterAllButton"
+        <button data-testid="filterAllButton" style="margin-left: 0;"
                 @click="filter('allTasks')"
                 class="orangeButton"
         >all
@@ -15,22 +15,45 @@
                 class="orangeButton"
         >active
         </button>
+
+        <button data-testid="clearCompletedButton" style="margin-left: auto;"
+                @click="clearCompletedTasks"
+                class="redButton"
+        >Clear completed
+        </button>
+        <button data-testid="clearAllButton"
+                @click="clearAllTasks"
+                class="redButton"
+        >Clear all
+        </button>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useStore } from 'vuex';
 
 export default defineComponent({
     name: 'TaskListButtonBar',
     emits: ['filter'],
     setup(props, {emit}) {
+        const store = useStore();
 
-        function filter(filterType: string) {
+        function filter(filterType: string): void {
             emit('filter', filterType);
         }
 
+        function clearAllTasks(): void {
+            store.dispatch('clearAllTasks');
+        }
+
+        function clearCompletedTasks(): void {
+            store.dispatch('clearCompletedTasks');
+        }
+
         return {
+            clearAllTasks,
+            clearCompletedTasks,
             filter
         };
     }
--- tests/unit/tasklistbuttonbar.spec.ts
+++ tests/unit/tasklistbuttonbar.spec.ts
@@ -2,6 +2,12 @@ import { shallowMount } from '@vue/test-utils';
 import TaskListButtonBar from '@/components/TaskListButtonBar.vue';
 
 describe('Tests for task list button bar Component', () => {
+    const $store = {
+        state: {
+            tasks: []
+        },
+        dispatch: jest.fn()
+    };
 
     it('Emit event: filter all tasks', async () => {
         const wrapper = shallowMount(TaskListButtonBar);
@@ -34,5 +40,37 @@ describe('Tests for task list button bar Component', () => {
         expect(wrapper.emitted()).toHaveProperty('filter');
         const filterEvent = wrapper.emitted().filter;
         expect(filterEvent[0]).toContainEqual('activeTasks');
+    });
+
+    it('Clear all tasks', async () => {
+        const wrapper = shallowMount(TaskListButtonBar, {
+            global: {
+                provide: {
+                    store: $store
+                }
+            }
+        });
+
+        const clearAllButton = wrapper.find('[data-testid="clearAllButton"]');
+        await clearAllButton.trigger('click');
+
+        expect($store.dispatch).toHaveBeenCalled();
+        expect($store.dispatch).toHaveBeenCalledWith('clearAllTasks');
+    });
+
+    it('Clear completed tasks', async () => {
+        const wrapper = shallowMount(TaskListButtonBar, {
+            global: {
+                provide: {
+                    store: $store
+                }
+            }
+        });
+
+        const clearCompletedButton = wrapper.find('[data-testid="clearCompletedButton"]');
+        await clearCompletedButton.trigger('click');
+
+        expect($store.dispatch).toHaveBeenCalled();
+        expect($store.dispatch).toHaveBeenCalledWith('clearCompletedTasks');
     });
 });
